e3e26c1797814e6be866e1ce103b4e068f4235dd
malindujayawickrama462 <168506653+malindujayawickrama462@users.noreply.github.com>
2025-12-03 16:38:00 +0530

    authcontroller and user model created

---
+++ backend/controllers/authController.js
@@ -0,0 +1,71 @@
+import { User } from "../model/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//Register User
+export const registerUser = async(req,res)=>{
+    try{
+        const{name,email,password,role} = req.body;
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                message : "Email already exit"
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+        const newUser = new User({
+            name,
+            email,
+            password:hashedPassword,
+            role
+        });
+        await newUser.save();
+        res.status(201).json({
+            message:"User registered Successfully"
+        });
+    }catch(err){
+        res.status(500).json({
+            message:err.message
+        });
+    }
+};
+
+//Login user
+export const LoginUser = async(req,res)=>{
+    try{
+        const{email,password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                message:"Invalid email or password"
+            });
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Invalid e,ail or password"
+            });
+        }
+        const token = jwt.sign(
+            {id:user._id,role:user.role},
+            process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        );
+        res.status(201).json({
+            message:"Login Successful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+            
+        });
+    }catch(err){
+        res.status(500).json({
+            message:err.message
+        });
+    }
+};
