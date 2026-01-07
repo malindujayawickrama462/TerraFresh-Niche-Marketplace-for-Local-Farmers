a7d575062866c4bc327a7a4ef6321fefdb90a41d
malindujayawickrama462 <168506653+malindujayawickrama462@users.noreply.github.com>
2026-01-07 16:08:40 +0530

    nav bar created

--- frontend/terrafresh/src/App.tsx
+++ frontend/terrafresh/src/App.tsx
@@ -1,11 +1,17 @@
+import NavBar from './components/NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
+
 
 function App() {
   
 
   return (
-   <LandingPage />
-  )
+    <BrowserRouter>
+    <NavBar/>
+    <LandingPage/>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ frontend/terrafresh/src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage(){
+    return(
+          <div className="bg-green-50 min-h-screen">
+            <div></div>
+          </div>
+    );
+}
